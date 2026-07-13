7b49f45f0d1c4d6b834ca2d04f674e271301eaa4
ferdimen <120120395+ferdimen@users.noreply.github.com>
2026-07-13 14:24:14 +0300

    Update footer.js

--- data/footer.js
+++ data/footer.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const ay = bugun.getMonth() + 1; // 1 = Ocak, 7 = Temmuz
 
         // Sabit, her gün görünecek varsayılan metniniz
-        const varsayilanMesaj = "<b>Ferdimen.</b> Adı yalnızlığından gelen kahraman | Tüm Hakları Saklıdır.";
+        const varsayilanMesaj = "<b>Ferdimen.</b> Vazgeçtiklerim, sizin bir ömür aradıklarınızdır. | Tüm Hakları Saklıdır.";
         
         let ozelMesaj = "";
 
